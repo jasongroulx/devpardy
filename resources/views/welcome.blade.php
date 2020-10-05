69185d44111d10d5194a9559270a2197ee9b9cb7
Jason Groulx <groulx.jason@gmail.com>
2020-10-05 09:53:49 -0400

    Installed Vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src="{{ asset('js/app.js')}}"></script>
 </body>
 </html>
